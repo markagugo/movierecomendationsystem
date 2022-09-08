12541a20bddc03a1fe50c5871d4e9ec55cd7f4ab
missmbali <missmbalisithole@gmail.com>
2022-09-08 04:51:58 +0100

    themoviesreco

--- static/recommend.js
+++ static/recommend.js
@@ -11,7 +11,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = '7fee067b60fd14ed0bd0013b0863045f';
+    var my_api_key = '3183d5fe72fee6fe9c96f689cc05e209';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -25,7 +25,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = '7fee067b60fd14ed0bd0013b0863045f';
+  var my_api_key = '3183d5fe72fee6fe9c96f689cc05e209';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
